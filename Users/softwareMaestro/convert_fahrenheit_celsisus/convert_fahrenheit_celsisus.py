19f93c45a1e044713b946ce71e3a2bb56efaf5a2
Jaehyeong Kim <imn00133@gmail.com>
2019-04-29 11:05:45 +0000

    softwareMaestro submitted his exercise

---
+++ Users/softwareMaestro/convert_fahrenheit_celsisus/convert_fahrenheit_celsisus.py
@@ -0,0 +1,3 @@
+#-*- coding: utf-8 -*-
+inputNumber=float(input("변환할 화씨온도(℉를) 입력하십시오: "))
+print("%0.2f℉는 %0.2f℃입니다." %(inputNumber, (inputNumber-32)*5/9))
---
+++ Users/softwareMaestro/vending_machine/vending_machine.py
@@ -0,0 +1,47 @@
+int inputMoney = 0;
+
+
+for(;;){
+print("돈을 넣으세요: ");
+scan("%d", inputMoney);
+
+printf("
+|1| 블랙커피 (100원)
+|2| 밀크커피 (150원)
+|3| 고급커피 (200원)
+|4| 거스름돈 계산 (~~원)
+현재 잔여 금액 >> ~~%d
+", inputMoney);
+}
+
+
+}
+
+
+
+
+
+
+int _menu(string option){
+if (option == "coffee" || option == "all"){
+printf(
+|1| 블랙커피 (100원)
+|2| 밀크커피 (150원)
+|3| 고급커피 (200원)
+)
+
+}
+if (option == "juice" || option == "all"){
+printf(
+|1| 사과쥬뜨 ><(100원)
+|2| 꼬치쥬뜨 데헷 (150원)
+|3| 천성쥬스 (999999원)
+)
+}
+
+~~~~ error return~ 필요~~ 
+
+추가추가 필요
+
+
+}
