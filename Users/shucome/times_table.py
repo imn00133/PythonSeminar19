7ffeb94af5f68a61e0aeb000d5a98eb92be0536a
JaeHyeong Kim <imn00133@gmail.com>
2019-05-20 11:58:49 +0000

    add comment by Jaehyeong

--- Users/shucome/times_table.py
+++ Users/shucome/times_table.py
@@ -3,3 +3,6 @@ for first in range(2, 10):
         print("%d * %d = %2d" % (first, second, first * second), end=" ")
         continue
     print(" ")
+
+# 잘하셨으나, 4번째 줄의 continue는 필요없는 문장입니다.
+# 수고하셨습니다. 주강사 김재형
