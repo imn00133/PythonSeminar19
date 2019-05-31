c000b23ed5b16d3d8988ff5b3c34a4c76d084a46
JaeHyeong Kim <imn00133@gmail.com>
2019-05-31 12:21:46 +0000

    cuz left comment

--- Users/cuz/vendig_machine/vendig_machine.py
+++ Users/cuz/vendig_machine/vendig_machine.py
@@ -12,32 +12,33 @@ while True:
     select_value = int(input("뽑을 물품을 골라주세요: "))-1
     if select_value < 0:
         break
-    if 0 <= select_value < len(item_list)+len(control_list)
+    # -->> 수정하였습니다. 하지만 돈 넣기와 거스름돈을 선택을 하면 자동으로 프로그램이 꺼지고맙니다.
+    if 0 <= select_value < len(item_list) + len(control_list):
         if input_money >= item_list[select_value][1]:
             input_money = input_money - item_list[select_value][1]
             print("%s가 나왔습니다." % item_list[select_value][0])
         else:
             print("돈이 부족합니다")
+    # 꺼지는 이유에 대해서 설명해드리겠습니다.
+    # 현재, 이 부분에서 item_list와 control_list를 동시에 확인하고 있습니다.
+    # 만일, 4를 입력했다고 하겠습니다.
+    # 그렇다면, 다음과 같은 문제가 발생합니다. 4는 0 <= select_value < len~~에 포함됨으로
+    # 이 수행문쪽으로 들어옵니다.
+    # 그런데, item_list를 보면 index 4는 존재하지 않습니다.
+    # 따라서 오류(out of index)가 나면서 오류가 발생하는 것입니다.
+    # 이에 대한 해결이 43번째 주석입니다.
+    # len(item_list)와 len(control_list)를 합쳐서 계산하는 것이 아닌,
+    # len(item_list)를 조건으로 하는 if문 하나, control_list만 계산할 수 있는 elif하나를 만들어야 합니다.
     else:
         print("물품번호를 잘못 입력하였습니다.")
     print("돈을 반환합니다.: %d" % input_money)
-    if select_value == 3:
+    elif select_value == 3:
         plus_money = int(input("돈을 더 넣어주세요: "))
-
-# 4번 줄에 돈을 넣는 부분이 무한 루프 안에 있어 돈을 계속 넣고 있습니다. =>
-# 이 부분에서 다시 처음 돈을 넣어주세요로 어떻게 전환할지 잘 모르겠네요 ㅠㅠ =>
+        plus_money += input_money
 
 # 지금 9번째 줄을 보시면, "4번에 돈 입력"이 있는 것을 볼 수 있습니다.
 # 따라서 4를 입력했을 때(elif를 사용하여),
 # input_money = int(input("돈을 넣어주세요: "))가 출력되도록 만들어주시면 됩니다.
-
-# -->> 리스트를 만들었습니다. 그리고 돈입력과 거스름돈을 위해 select_value 값을 지정하려고 하였습니다.
-# -->> 하지만 15번 줄부터 바로 select_value 값의 조건이 되지않아 오류가 뜨고맙니다.
-# -->> 그래서 길이를 늘려주었습니다. 그 다음엔 16번 줄의 물건출력 및 돈감소가 오류가 뜹니다.
-# -->> 다른 좋은 방법을 생각하기엔 시간이 너무 부족하네요. ㅠㅠ 다음에 다른 방법을 찾아야겠습니다.
-
-# 현재 오류는 15번째 줄에 ":"이 빠져서 그렇습니다. select_value의 값의 오류와는 상관없는 문법 오류입니다.
-# 실행시켰을 때, syntax error라고 뜨면, 이는 :이나 ", 들여쓰기등이 잘못되어서 생기는 오류가 생각하시면 됩니다.
 
 # 15번째 줄 아래의 16~20번째 줄은 물품을 선택했을 때 출력하는 구간입니다.
 # 따라서 15번째 줄과 21번을 if-else로 묶는 것 보다는
